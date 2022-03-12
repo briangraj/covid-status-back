c0a7c3da8d4827255e4f547ab675a8c3b44b7ff4
Brian Grajeda <brian.graj@gmail.com>
2022-03-12 14:35:55 -0300

    chore(tests): create tests for GET /covid/deaths

--- test/integration/stat_flows_test.rb
+++ test/integration/stat_flows_test.rb
@@ -20,4 +20,24 @@ class StatFlowsTest < ActionDispatch::IntegrationTest
 
       assert_equal 2, response["count"]
     end
+
+    test "GET /covid/deaths is recognized" do
+      assert_recognizes(
+        { controller: "stats", action: "deaths" },
+        { path: "covid/deaths", method: :get }
+      )
+    end
+
+    test "GET /covid/deaths responds 200" do
+      get covid_deaths_path
+
+      assert_response :ok
+    end
+
+    test "GET /covid/deaths responds a number" do
+      get covid_deaths_path
+      response = JSON.parse @response.body
+
+      assert_equal 1, response["count"]
+    end
 end
