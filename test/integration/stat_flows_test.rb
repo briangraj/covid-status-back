6a43421b36677465c96bba7cd0aa3f42319cb504
Brian Grajeda <brian.graj@gmail.com>
2022-03-12 15:42:00 -0300

    refactor(tests): get expected values from the db instead of being hardcoded. And add fixture value

--- test/integration/stat_flows_test.rb
+++ test/integration/stat_flows_test.rb
@@ -15,10 +15,12 @@ class StatFlowsTest < ActionDispatch::IntegrationTest
     end
 
     test "GET /covid/total responds a number" do
+      total_count = Case.count
+
       get covid_total_path
       response = JSON.parse @response.body
 
-      assert_equal 2, response["count"]
+      assert_equal total_count, response["count"]
     end
 
     test "GET /covid/deaths is recognized" do
@@ -35,9 +37,11 @@ class StatFlowsTest < ActionDispatch::IntegrationTest
     end
 
     test "GET /covid/deaths responds a number" do
+      deaths_count = Case.where.not(death_date: nil).count
+
       get covid_deaths_path
       response = JSON.parse @response.body
 
-      assert_equal 0, response["count"]
+      assert_equal deaths_count, response["count"]
     end
 end
