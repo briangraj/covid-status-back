28367d95a60a8a31ecbaee675ba57189e3c7744e
Brian Grajeda <brian.graj@gmail.com>
2022-03-12 14:50:52 -0300

    feat(endpoints): GET /covid/deaths returns count without filters

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,4 +4,11 @@ class StatsController < ApplicationController
 
     render json: { count: @count }, status: :ok
   end
+
+  def deaths
+    @count = Case.where.not(death_date: nil)
+                 .count
+
+    render json: { count: @count }, status: :ok
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   scope "/covid" do
     get "total", to: "stats#total", as: "covid_total"
+    get "deaths", to: "stats#deaths", as: "covid_deaths"
   end
 end
