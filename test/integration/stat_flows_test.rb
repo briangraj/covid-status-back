5a19d7ac95e87226e99d7db8a18cba006793242b
Brian Grajeda <brian.graj@gmail.com>
2022-03-12 15:49:49 -0300

    chore(tests): add tests and update fixture for gender filter

--- test/integration/stat_flows_test.rb
+++ test/integration/stat_flows_test.rb
@@ -23,6 +23,15 @@ class StatFlowsTest < ActionDispatch::IntegrationTest
       assert_equal total_count, response["count"]
     end
 
+    test "GET /covid/total should filter by gender" do
+      total_count = Case.where(gender: "M").count
+
+      get(covid_total_path + "?gender=M")
+      response = JSON.parse @response.body
+
+      assert_equal total_count, response["count"]
+    end
+
     test "GET /covid/deaths is recognized" do
       assert_recognizes(
         { controller: "stats", action: "deaths" },
@@ -42,6 +51,17 @@ class StatFlowsTest < ActionDispatch::IntegrationTest
       get covid_deaths_path
       response = JSON.parse @response.body
 
+      assert_equal deaths_count, response["count"]
+    end
+
+    test "GET /covid/deaths should filter by gender" do
+      deaths_count = Case.where.not(death_date: nil)
+                         .where(gender: "M")
+                         .count
+      puts deaths_count
+      get(covid_deaths_path + "?gender=M")
+      response = JSON.parse @response.body
+      puts response["count"]
       assert_equal deaths_count, response["count"]
     end
 end
