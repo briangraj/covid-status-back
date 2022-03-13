fc3bb2adaa364f3220501a8d64e747df946c0f78
Brian Grajeda <brian.graj@gmail.com>
2022-03-13 16:12:34 -0300

    feat(endpoints): accept date_from and date_to query params

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,13 +1,13 @@
 class StatsController < ApplicationController
   def total
-    allowed_params = params.permit Case.total_allowed_params
+    allowed_params = rename_keys cases_query_param, { "date_from" => "diagnosis_date_from", "date_to" => "diagnosis_date_to" }
     @count = Case.where_with_scopes(allowed_params).count
 
     render json: { count: @count }, status: :ok
   end
 
   def deaths
-    allowed_params = params.permit Case.deaths_allowed_params
+    allowed_params = rename_keys cases_query_param, { "date_from" => "death_date_from", "date_to" => "death_date_to" }
     @count = Case.where_with_scopes(allowed_params)
                  .where.not(death_date: nil)
                  .count
@@ -18,6 +18,12 @@ class StatsController < ApplicationController
   private
   def cases_query_param
     # TODO verify type or cast???
-    params.permit(:gender, :age_from, :age_to)
+    params.permit(:gender, :age_from, :age_to, :date_from, :date_to)
+  end
+
+  def rename_keys(params, renames_hash)
+    params.transform_keys do |key|
+      renames_hash.key?(key) ? renames_hash[key] : key
+    end
   end
 end
