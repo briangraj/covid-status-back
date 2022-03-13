4d46d012abc03d449888019865dcd2f2626db3cb
Brian Grajeda <brian.graj@gmail.com>
2022-03-13 11:40:49 -0300

    chore(tests): add tests for age_from and age_to filters

--- test/integration/stat_flows_test.rb
+++ test/integration/stat_flows_test.rb
@@ -32,6 +32,24 @@ class StatFlowsTest < ActionDispatch::IntegrationTest
       assert_equal total_count, response["count"]
     end
 
+    test "GET /covid/total should filter by age_from" do
+      total_count = Case.age_from(30).count
+
+      get(covid_total_path + "?age_from=30")
+      response = JSON.parse @response.body
+
+      assert_equal total_count, response["count"]
+    end
+
+    test "GET /covid/total should filter by age_to" do
+      total_count = Case.age_to(25).count
+
+      get(covid_total_path + "?age_to=25")
+      response = JSON.parse @response.body
+
+      assert_equal total_count, response["count"]
+    end
+
     test "GET /covid/deaths is recognized" do
       assert_recognizes(
         { controller: "stats", action: "deaths" },
@@ -60,6 +78,24 @@ class StatFlowsTest < ActionDispatch::IntegrationTest
                          .count
 
       get(covid_deaths_path + "?gender=M")
+      response = JSON.parse @response.body
+
+      assert_equal deaths_count, response["count"]
+    end
+
+    test "GET /covid/deaths should filter by age_from" do
+      deaths_count = Case.age_from(30).count
+
+      get(covid_deaths_path + "?age_from=30")
+      response = JSON.parse @response.body
+
+      assert_equal deaths_count, response["count"]
+    end
+
+    test "GET /covid/deaths should filter by age_to" do
+      deaths_count = Case.age_to(25).count
+
+      get(covid_deaths_path + "?age_to=25")
       response = JSON.parse @response.body
 
       assert_equal deaths_count, response["count"]
