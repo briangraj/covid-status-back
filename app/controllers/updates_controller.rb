3de3db8d6bd85e1609af34740fe5d5498a1981dc
Brian Grajeda <brian.graj@gmail.com>
2022-04-01 21:38:42 -0300

    fix(endpoints): GET /covid/updates responds if there is no record in Update

--- app/controllers/updates_controller.rb
+++ app/controllers/updates_controller.rb
@@ -7,11 +7,19 @@ class UpdatesController < ApplicationController
   def show
     @update = Update.select(:last_load_date, :updated_records).last
 
-    # TODO generalize for any object
-    msg = {
-      lastLoadDate: @update.last_load_date,
-      updatedRecords: @update.updated_records
-    }
+    # TODO retornar un NO content para decir que no existe registro
+    if @update.nil?
+      msg = {
+        lastLoadDate: "Never",
+        updatedRecords: "None"
+      }
+    else
+      # TODO generalize for any object
+      msg = {
+        lastLoadDate: @update.last_load_date,
+        updatedRecords: @update.updated_records
+      }
+    end
 
     render json: msg, status: :ok
   end
