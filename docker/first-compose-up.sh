0d515e7358187fbf6157fee104b31df24dcea6da
Brian Grajeda <brian.graj@gmail.com>
2022-04-10 19:14:13 -0300

    chore(docker): create files with common commands

---
+++ docker/first-compose-up.sh
@@ -0,0 +1,5 @@
+docker-compose up
+
+docker-compose run csb rake db:create
+
+docker-compose run csb rake db:migrate
---
+++ docker/push-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# docker tag <existing-image> <hub-user>/<repo-name>[:<tag>]
+docker tag csb:latest briangraj/csb:latest
+
+# docker push <hub-user>/<repo-name>:<tag>
+docker push briangraj/csb:latest
